fed5b06c800c48155532f264d3d1d836299fafa7
gingerbeardman <matt.sephton@gmail.com>
2012-01-06 20:11:24 +0000

    update compose demo with text and url arguments

--- iPhone/Twitter/example/www/demo.js
+++ iPhone/Twitter/example/www/demo.js
@@ -12,58 +12,60 @@ TwitterDemo = {
         return document.getElementById(id);
     },
     
-    log:function(s){
-        TwitterDemo.$("log").innerHTML = s;
-    },
+log:function(s){
+    TwitterDemo.$("log").innerHTML = s;
+},
     
-    setup:function(){
-        var tests = ["isAvailable", "isSetup", "tweet", "compose", "timeline", "mentions"];
-        for(var i=0, l=tests.length; i<l; i++){
-            this.$(tests[i]).onclick = this[tests[i]];
-        }
-    },
-    
-    isAvailable:function(){
-        TwitterDemo.log("wait..");
-        window.plugins.twitter.isTwitterAvailable(function(r){
-            TwitterDemo.log("twitter available? " + r);
-        });        
-    },
-    
-    isSetup:function(){
-        TwitterDemo.log("wait..");
-        window.plugins.twitter.isTwitterSetup(function(r){
-            TwitterDemo.log("twitter configured? " + r);
-        });
-    },
-    
-    tweet:function(){
-        TwitterDemo.log("wait..");
-        window.plugins.twitter.sendTweet(
-            function(s){ TwitterDemo.log("tweet success"); }, 
-            function(e){ TwitterDemo.log("tweet failure: " + e); }, 
-            "Tweety Poo", 
-            "https://github.com/brianantonelli", 
-            "http://zomgdinosaurs.com/zomg.jpg");
-    },
-    
-    compose: function() {
-        TwitterDemo.log("wait..");
-          window.plugins.twitter.composeTweet();
-    },
-    
-    timeline:function(){
-        TwitterDemo.log("wait..");
-        window.plugins.twitter.getPublicTimeline(
-            function(s){ TwitterDemo.log("timeline success: " + JSON.stringify(s)); }, 
-            function(e){ TwitterDemo.log("timeline failure: " + e); });
-    },
-    
-    mentions:function(){
-        TwitterDemo.log("wait..");
-        window.plugins.twitter.getMentions(
-            function(s){ TwitterDemo.log("mentions success: " + JSON.stringify(s)); }, 
-            function(e){ TwitterDemo.log("mentions failure: " + e); });
+setup:function(){
+    var tests = ["isAvailable", "isSetup", "tweet", "compose", "timeline", "mentions"];
+    for(var i=0, l=tests.length; i<l; i++){
+        this.$(tests[i]).onclick = this[tests[i]];
     }
+},
+    
+isAvailable:function(){
+    TwitterDemo.log("wait..");
+    window.plugins.twitter.isTwitterAvailable(function(r){
+                                              TwitterDemo.log("twitter available? " + r);
+                                              });        
+},
+    
+isSetup:function(){
+    TwitterDemo.log("wait..");
+    window.plugins.twitter.isTwitterSetup(function(r){
+                                          TwitterDemo.log("twitter configured? " + r);
+                                          });
+},
+    
+tweet:function(){
+    TwitterDemo.log("wait..");
+    window.plugins.twitter.sendTweet(
+                                     function(s){ TwitterDemo.log("tweet success"); }, 
+                                     function(e){ TwitterDemo.log("tweet failure: " + e); }, 
+                                     "Tweety Poo", 
+                                     "https://github.com/brianantonelli", 
+                                     "http://zomgdinosaurs.com/zomg.jpg");
+},
+    
+compose: function() {
+    TwitterDemo.log("wait..");
+    window.plugins.twitter.composeTweet(
+                                        "Tweet Text", 
+                                        "https://github.com/gingerbeardman");
+},
+    
+timeline:function(){
+    TwitterDemo.log("wait..");
+    window.plugins.twitter.getPublicTimeline(
+                                             function(s){ TwitterDemo.log("timeline success: " + JSON.stringify(s)); }, 
+                                             function(e){ TwitterDemo.log("timeline failure: " + e); });
+},
+    
+mentions:function(){
+    TwitterDemo.log("wait..");
+    window.plugins.twitter.getMentions(
+                                       function(s){ TwitterDemo.log("mentions success: " + JSON.stringify(s)); }, 
+                                       function(e){ TwitterDemo.log("mentions failure: " + e); });
+}
     
 };
